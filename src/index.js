799c7dbacd464b1a5a7f9e7c5aa6db8ffe43b58a
usefproj <sevoehabr@gmail.com>
2025-03-15 13:39:19 +0200

    error

--- src/index.js
+++ src/index.js
@@ -19,9 +19,19 @@ app.get("/", async (req, res) => {
 
 app.get("/users", async (req, res) => {
   // Write your logic here
-  const response = await fetch("https://dummyjson.com/users");
-  const users = await response.json();
-  res.json(users);
+  try {
+    const response = await fetch("https://dummyjson.com/users");
+    if (!response.ok) {
+      throw new Error(
+        `Network response was not OK (status ${response.status})`
+      );
+    }
+    const users = await response.json();
+    res.json(users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).send("Error fetching users");
+  }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
