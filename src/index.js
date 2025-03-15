a1bccd2104236cc98a75eee056de1708fd5900e2
usefproj <sevoehabr@gmail.com>
2025-03-15 13:21:12 +0200

    fetching

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const path = require('path')
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 // setup the ability to see into response bodies
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // setup the express assets path
-app.use('/', express.static(path.join(__dirname, '../client')))
+app.use("/", express.static(path.join(__dirname, "../client")));
 
 // API calls ------------------------------------------------------------------------------------
-app.get('/', async (req, res) => {
-    res.sendFile(path.join(__dirname, './client/pages/user.html'));
-})
+app.get("/", async (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/pages/user.html"));
+});
 
-app.get('/users', async (req, res) => {
-    // Write your logic here
-})
+app.get("/users", async (req, res) => {
+  // Write your logic here
+  const response = await fetch("https://dummyjson.com/users");
+  const users = await response.json();
+  res.json(users);
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
